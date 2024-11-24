dd004c23266cb8d5293a55a51ec66ec1961d1e7e
kambgz <kambgz@gmail.com>
2024-11-24 09:51:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-function playMusic() {
-  const music = document.getElementById('birthday-music');
-  if (music.paused) {
-    music.play();
-    alert('Musik dimulai, Selamat menikmati 🎉');
-  } else {
-    music.pause();
-    alert('Musik dihentikan');
-  }
-}
+
